be889a3e4239344dbb20e3a9da17e3583b5cc958
eac3329 <eac3329@gmail.com>
2026-02-24 01:42:47 +0600

    implement filtering logic

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ function renderJobs() {
     for (let i = 0; i < jobsData.length; i++) {
         let job = jobsData[i];
         const jobHTML = `
+        
+<button onclick="deleteJob(${job.id})" class="absolute top-6 right-6 delete-btn">
+    <svg xmlns="http://www.w3.org/2001/XMLSchema/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+    </svg>
+</button>
+
+
             <div class="p-6 relative">
                 <h4 class="text-[#1E3A8A] font-bold text-xl mb-1">${job.company}</h4>
                 <p class="text-slate-700 font-semibold mb-2">${job.role}</p>
